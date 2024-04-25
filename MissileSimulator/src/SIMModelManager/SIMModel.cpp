9afbe19ac01180ff844d56df8a74c5d4b0b4abd3
kyyneogs <oks990404@gmail.com>
2024-04-25 15:41:17 +0900

    FIX: 유도탄모의기 속도 버그 해결

--- MissileSimulator/src/SIMModelManager/SIMModel.cpp
+++ MissileSimulator/src/SIMModelManager/SIMModel.cpp
@@ -4,6 +4,9 @@
 
 #include <iostream>
 
+constexpr float kSimulateHz = 10.0;
+constexpr float kHitRange = 300.0;
+
 SIMModel::SIMModel(SharedData* data) : mData(data) {
   targetX = 0;
   targetY = 0;
@@ -24,8 +27,8 @@ void SIMModel::updateTargetInfo(float x, float y) {
 }
 
 void SIMModel::updateMissilePosition() {
-  int mslX = mData->x;
-  int mslY = mData->y;
+  float mslX = mData->x;
+  float mslY = mData->y;
 
   float deltaX, deltaY;
   float theta;
@@ -34,7 +37,9 @@ void SIMModel::updateMissilePosition() {
   deltaY = abs(targetY - mslY);
 
   // hit process
-  if (deltaX <= 30.0 && deltaY <= 30.0) {
+  if (deltaX <= kHitRange && deltaY <= kHitRange) {
+    mData->x = targetX;
+    mData->y = targetY;
     mData->MissileBomb = 2;
     return;
   }
@@ -42,65 +47,16 @@ void SIMModel::updateMissilePosition() {
   theta = abs(atan(deltaY / deltaX));
 
   if (mslX < targetX)
-    mslX = mslX + (mslSpeed * cos(theta) / 100);
+    mslX = mslX + (mslSpeed * cos(theta) / kSimulateHz);
   else
-    mslX = mslX - (mslSpeed * cos(theta) / 100);
+    mslX = mslX - (mslSpeed * cos(theta) / kSimulateHz);
 
   if (mslY < targetY)
-    mslY = mslY + (mslSpeed * sin(theta) / 100);
+    mslY = mslY + (mslSpeed * sin(theta) / kSimulateHz);
   else
-    mslY = mslY - (mslSpeed * sin(theta) / 100);
+    mslY = mslY - (mslSpeed * sin(theta) / kSimulateHz);
 
   // update real data position
   mData->x = mslX;
   mData->y = mslY;
 }
-
-// void SIMModel::start() {
-//   currentX = initX;
-//   currentY = initY;
-//
-//   StartFlag = true;
-//   MissileBomb = 1;
-//
-//   setMissilePosition(currentX, currentY,
-//                      1700);
-//
-//   isThreadRunning = true;
-//   simThread =
-//       new std::thread(std::bind(&SIMModel::threatSimulationThread, this));
-// }
-
-// void SIMModel::stop() {
-//   isThreadRunning = false;
-//
-//   if (simThread) {
-//     simThread->join();
-//     delete simThread;
-//     simThread = nullptr;
-//   }
-// }
-
-// void SIMModel::threatSimulationThread() {
-//   while (isThreadRunning) {
-//     RecvThreatPosition(targetX, targetY, targetspeed, 0);
-//     updateMissilePosition();
-//
-//     // the code below may be done by updating the target destination
-//     // currentX, currentY
-//
-//     // check the range and end condition
-//     if ((abs(currentX - targetX) <= 30) && (abs(currentY - targetY) <= 30)) {
-//       MissileBomb = 2;  // success bomb
-//       std::cout << "¸¶Áö¸· DATA : Threat position : ( ¹Ì»çÀÏ X : " <<
-//       currentX
-//                 << ", ¹Ì»çÀÏ Y : " << currentY << ", Å¸°Ù X : " << targetX
-//                 << ", Å¸°Ù  Y : " << targetY << ", ÆøÆÈÇÔ -> " << MissileBomb
-//                 << ")" << std::endl;  // for test
-//       isThreadRunning = false;
-//       break;
-//     }
-//
-//     std::this_thread::sleep_for(std::chrono::milliseconds(10));
-//   }
-// }
